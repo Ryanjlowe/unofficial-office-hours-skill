80c0940acad79f627a82f9cb25b1b1b82695488a
Ryan Lowe <ryanjlowe@Ryans-iMac.local>
2022-01-26 11:23:28 -0700

    Better youtube downloader

--- youtube-download/index.js
+++ youtube-download/index.js
@@ -1,46 +1,50 @@
-
 const stream = require('stream');
-const youtubedl = require('youtube-dl');
 const AWS = require('aws-sdk');
 
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 const bucket = process.env.BUCKET_NAME;
 
 exports.handler = async (event) => {
 
   console.log(event);
+  const passthrough = new stream.PassThrough();
 
-  const key = "unprocessed/" + event.youtubeVideName + ".mp4";
-  const passtrough = new stream.PassThrough();
+  var info = await ytdl.getInfo(event.youtubeUrl);
+  var name = info.player_response.videoDetails.title.replace(/[^a-zA-Z0-9]+/g, "-");
 
-  const dl = youtubedl(event.youtubeUrl, ['--format=best[ext=mp4]'], {maxBuffer: Infinity});
-  dl.pipe(passtrough); // write video to the pass-through stream
+  const key = `unprocessed/${name}.mp4`;
 
+  ytdl(event.youtubeUrl)
+    .pipe(passthrough);
 
-  const p = new Promise();
+  const p = new Promise((resolve, reject) => {
 
-  const upload = new AWS.S3.ManagedUpload({
-    params: {
-      Bucket: bucket,
-      Key: key,
-      Body: passtrough
-    },
-    partSize: 1024 * 1024 * 64 // 64 MB in bytes
-  });
-  upload.send((err) => {
-    if (err) {
-      console.log('error', err);
-    } else {
-      console.log('done');
+    const upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: bucket,
+        Key: key,
+        Body: passthrough
+      },
+      partSize: 1024 * 1024 * 64 // 64 MB in bytes
+    });
+    upload.send((err) => {
+      if (err) {
+        console.log('error', err);
+        reject(err);
+      } else {
+        console.log('done');
 
-      p.resolve( {
-        "mediaS3Location": {
-          "bucket": bucket,
-          "key": key
-        }
-      });
-    }
-  });
+       resolve( {
+          "mediaS3Location": {
+            "bucket": bucket,
+            "key": key
+          }
+        });
+      }
+    });
+  })
 
   return p;
 };
