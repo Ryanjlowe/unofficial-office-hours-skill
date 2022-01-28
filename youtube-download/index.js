cba341b5f4d33a4ba7c4a4a31ff575537b3a3e96
Ryan Lowe <ryanjlowe@Ryans-iMac.local>
2022-01-28 10:22:55 -0700

    Updated youtube download, added dynamodb table to capture metadata

--- youtube-download/index.js
+++ youtube-download/index.js
@@ -5,46 +5,78 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 const bucket = process.env.BUCKET_NAME;
+const table_name = process.env.TABLE_NAME;
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
 
   console.log(event);
   const passthrough = new stream.PassThrough();
 
-  var info = await ytdl.getInfo(event.youtubeUrl);
-  var name = info.player_response.videoDetails.title.replace(/[^a-zA-Z0-9]+/g, "-");
+  const info = await ytdl.getInfo(event.youtubeUrl);
+  const title = info.videoDetails.title;
+  const name = title.replace(/[^a-zA-Z0-9]+/g, "-");
+  const publishDate = info.videoDetails.publishDate;
+  const id = info.videoDetails.videoId;
+  const description = info.videoDetails.description;
+  const lengthSeconds = info.videoDetails.lengthSeconds;
+
+  // Example to ensure the format is available
+  // let format = ytdl.chooseFormat(info.formats, { quality: '137' });
 
   const key = `unprocessed/${name}.mp4`;
 
-  ytdl(event.youtubeUrl)
-    .pipe(passthrough);
+  var params = {
+      TableName: table_name,
+      Item: {
+          "id":  id,
+          "title": title,
+          "name": name,
+          "key": key,
+          "publishDate": publishDate,
+          "description": description,
+          "lengthSeconds": lengthSeconds
+      }
+  };
 
-  const p = new Promise((resolve, reject) => {
+  return docClient.put(params).promise()
+    .then(() => {
 
-    const upload = new AWS.S3.ManagedUpload({
-      params: {
-        Bucket: bucket,
-        Key: key,
-        Body: passthrough
-      },
-      partSize: 1024 * 1024 * 64 // 64 MB in bytes
-    });
-    upload.send((err) => {
-      if (err) {
-        console.log('error', err);
-        reject(err);
-      } else {
-        console.log('done');
+      ytdl(event.youtubeUrl, { quality: '22' })
+        .pipe(passthrough);
 
-       resolve( {
-          "mediaS3Location": {
-            "bucket": bucket,
-            "key": key
+      const p = new Promise((resolve, reject) => {
+
+        const upload = new AWS.S3.ManagedUpload({
+          params: {
+            Bucket: bucket,
+            Key: key,
+            Body: passthrough
+          },
+          partSize: 1024 * 1024 * 64 // 64 MB in bytes
+        });
+        upload.send((err) => {
+          if (err) {
+            console.log('error', err);
+            reject(err);
+          } else {
+            console.log('done');
+
+           resolve( {
+              "mediaS3Location": {
+                "bucket": bucket,
+                "key": key
+              }
+            });
           }
         });
-      }
-    });
-  })
+      })
 
-  return p;
+      return p;
+    })
+    .catch((error) => {
+      console.error(error);
+      return {};
+    })
 };
