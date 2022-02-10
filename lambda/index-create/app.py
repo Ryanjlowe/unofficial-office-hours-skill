49978819d9422cc5baf3a44ba998274302ceaef5
Ryan Lowe <ryanjlowe@Ryans-iMac.local>
2022-02-10 13:23:40 -0700

    Refactoring lambda functions into a single folder, fixed the issue with deploying the opensearch cluster

--- lambda/index-create/app.py
+++ lambda/index-create/app.py
@@ -1,22 +1,13 @@
 from __future__ import print_function
 
 import boto3
-import certifi
 import json
 import os
-from aws_requests_auth.aws_auth import AWSRequestsAuth
-from opensearchpy import OpenSearch
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
 import logging
 import time
 import cfnresponse
-
-# Log level
-logging.basicConfig()
-logger = logging.getLogger()
-if os.getenv('LOG_LEVEL') == 'DEBUG':
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.INFO)
 
 # Parameters
 REGION = os.getenv('AWS_REGION', default='us-east-1')
@@ -30,45 +21,21 @@ FULL_EPISODE_INDEX = os.getenv('ES_EPISODE_INDEX', default='episodes')
 PARAGRAPHS_INDEX = os.getenv('ES_PARAGRAPH_INDEX', default='paragraphs')
 
 s3_client = boto3.client('s3')
-# Create the auth token for the sigv4 signature
-session = boto3.session.Session()
-credentials = session.get_credentials().get_frozen_credentials()
-awsauth = AWSRequestsAuth(
-    aws_access_key=credentials.access_key,
-    aws_secret_access_key=credentials.secret_key,
-    aws_token=credentials.token,
-    aws_host=esendpoint,
-    aws_region=REGION,
-    aws_service='es'
-)
-
-# # Connect to the elasticsearch cluster using aws authentication. The lambda function
-# # must have access in an IAM policy to the ES cluster.
-# es = Elasticsearch(
-#     hosts=[{'host': esendpoint, 'port': 443}],
-#     http_auth=awsauth,
-#     use_ssl=True,
-#     verify_certs=True,
-#     ca_certs=certifi.where(),
-#     timeout=120,
-#     connection_class=RequestsHttpConnection
-# )
 
 
 
-# Create the client with SSL/TLS enabled, but hostname verification disabled.
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, REGION, service, session_token=credentials.token)
+
 client = OpenSearch(
-    hosts = [{'host': osendpoint, 'port': 9200}],
-    http_compress = True, # enables gzip compression for request bodies
+    hosts = [{'host': osendpoint, 'port': 443}],
     http_auth = awsauth,
     use_ssl = True,
     verify_certs = True,
-    ca_certs=certifi.where(),
-    ssl_assert_hostname = False,
-    ssl_show_warn = False,
-    timeout=120,
-    connection_class=RequestsHttpConnection
+    connection_class = RequestsHttpConnection
 )
+
 
 
 
@@ -104,11 +71,11 @@ def create_episode_index():
     }
     '''
     start = time.time()
-    logger.info("mappings to create for index: " + mappings)
+    logging.info("mappings to create for index: " + mappings)
     res = client.indices.create(index=FULL_EPISODE_INDEX, body=mappings)
     round_trip = time.time() - start
-    logger.info(json.dumps(res, indent=2))
-    logger.info('REQUEST_TIME es_client.indices.create {:10.4f}'.format(round_trip))
+    logging.info(json.dumps(res, indent=2))
+    logging.info('REQUEST_TIME es_client.indices.create {:10.4f}'.format(round_trip))
 
 def create_paragraph_index():
     mappings = '''
@@ -133,32 +100,43 @@ def create_paragraph_index():
     }
     '''
     start = time.time()
-    logger.info("mappings to create for index: " + mappings)
+    logging.info("mappings to create for index: " + mappings)
     res = client.indices.create(index=PARAGRAPHS_INDEX, body=mappings)
     round_trip = time.time() - start
-    logger.info(json.dumps(res, indent=2))
-    logger.info('REQUEST_TIME es_client.indices.create {:10.4f}'.format(round_trip))
+    logging.info(json.dumps(res, indent=2))
+    logging.info('REQUEST_TIME es_client.indices.create {:10.4f}'.format(round_trip))
 
 
 def lambda_handler(event, context):
+    log_level = str(os.environ.get('LOG_LEVEL')).upper()
+    if log_level not in [
+                        'DEBUG', 'INFO',
+                        'WARNING', 'ERROR',
+                        'CRITICAL'
+                    ]:
+      log_level = 'DEBUG'
+    logging.getLogger().setLevel(log_level)
+
+    logging.info(event)
+
     try:
         if event['RequestType'] == 'Create':
             if not client.indices.exists(index=FULL_EPISODE_INDEX):
                 create_episode_index()
             else:
-                logger.info("index " + FULL_EPISODE_INDEX + " already exists. skipping index creation.")
+                logging.info("index " + FULL_EPISODE_INDEX + " already exists. skipping index creation.")
 
             if not client.indices.exists(index=PARAGRAPHS_INDEX):
                 create_paragraph_index()
             else:
-                logger.info("index " + FULL_EPISODE_INDEX + " already exists. skipping index creation.")
+                logging.info("index " + FULL_EPISODE_INDEX + " already exists. skipping index creation.")
 
-            cfnresponse.send(event, context, cfnresponse.SUCCESS, create_response)
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
 
         if event['RequestType'] == 'Update':
             cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
         elif event['RequestType'] == 'Delete':
             cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
     except:
-        logger.error("Error", exc_info=True)
+        logging.error("Error", exc_info=True)
         cfnresponse.send(event, context, cfnresponse.FAILED, {})
